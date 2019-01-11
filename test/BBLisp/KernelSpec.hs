af202dd5b7d27c380dbe6e4e583feb640a0da69b
Gavin Lam <me@gavin.hk>
2019-01-11 23:09:39 +0800

    Added tests for if syntactic form

--- test/BBLisp/KernelSpec.hs
+++ test/BBLisp/KernelSpec.hs
@@ -12,7 +12,7 @@ import Test.Hspec
 
 -- | Spec for `Kernel`.
 spec :: Spec
-spec =
+spec = do
     describe "str" $ do
         it "returns the string representation of true" $
             K.str [Boolean True] `shouldBe` Right (String "true")
@@ -34,6 +34,24 @@ spec =
             K.str strTestList `shouldBe` Right (String "1world.")
         it "returns empty string for zero arguments" $
             K.str [] `shouldBe` Right (String "")
+    describe "if'" $ do
+        it "evaluates and returns then when test is evaluated to true" $
+            snd <$> ifTest (Boolean True) `shouldBe` Right (String "42")
+        it "evaluates and returns then when test is not evaluated to false" $
+            snd <$> ifTest (Integer 0) `shouldBe` Right (String "42")
+        it "evaluates and returns else when test is evaluated to false" $
+            snd <$> ifTest (Boolean False) `shouldBe` Right (String "falsy")
+        it "returns error for too few arguments" $
+            K.if' K.primitives ifFewArgs `shouldBe` Left "Too few arguments to if"
+        it "returns error for too many arguments" $
+            K.if' K.primitives ifManyArgs `shouldBe` Left "Too many arguments to if"
   where
     piStr       = "3.1415926535"
     strTestList = [Integer 1, Symbol "world", String "."]
+    ifTest test = K.if' K.primitives
+        [ test
+        , List [Symbol "str", Integer 42]
+        , List [Symbol "str", Symbol "falsy"]
+        ]
+    ifFewArgs   = [Boolean True]
+    ifManyArgs  = [Boolean True, Integer 1, Integer 2, Integer 3]
